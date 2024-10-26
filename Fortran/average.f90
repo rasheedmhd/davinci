6589b58f13aebb84d8ebc146c9aa8c1305b940ef
rasheedmhd <starletgh@gmail.com>
2024-10-26 23:18:14 +0000

    string concatenation

--- Fortran/average.f90
+++ Fortran/average.f90
@@ -3,35 +3,35 @@
 !			than 100, followed by List_Len-Integer values
 ! Output:	The number of input values that are greater
 !			than  the average of all input values
+Program Average
+    Implicit None
 
-Implicit None
+    Integer Dimension(99) :: Int_List
+    Integer :: List_Len, Counter, Sum, Average, Result
 
-Integer Dimension(99) :: Int_List
-Integer :: List_Len, Counter, Sum, Average, Result
+    Result = 0
+    Sum = 0
 
-Result = 0
-Sum = 0
+    Read *, List_Len
+    If ((List_Len > 0) .AND. (List_Len < 100)) Then 
+        ! Read input data into an Array and compute its sum
+        Do Counter = 1, List_Len
+            Read *, Int_List(Counter)
+            Sum = Sum + Int_List(Counter)
+        End Do
 
-Read *, List_Len
-If ((List_Len > 0) .AND. (List_Len < 100)) Then 
-    ! Read input data into an Array and compute its sum
-    Do Counter = 1, List_Len
-        Read *, Int_List(Counter)
-        Sum = Sum + Int_List(Counter)
-    End Do
+        ! Compute the Average 
+        Average = Sum / List_Len
+        ! Count the values that are greater than the average 
+        Do Counter = 1, List_Len
+            If (Int_List(Counter) > Average) Then 
+                Result = Result + 1
+            End If
+        End Do 
 
-    ! Compute the Average 
-    Average = Sum / List_Len
-    ! Count the values that are greater than the average 
-    Do Counter = 1, List_Len
-        If (Int_List(Counter) > Average) Then 
-            Result = Result + 1
-        End If
-    End Do 
-
-    ! Print the Result 
-    Print *, 'Number of values > Average is:', Result
-Else
-    Print *, 'Error - List length value is not legal'
-End If
+        ! Print the Result 
+        Print *, 'Number of values > Average is:', Result
+    Else
+        Print *, 'Error - List length value is not legal'
+    End If
 End Program Average
--- Fortran/tg.f90
+++ Fortran/tg.f90
@@ -1,6 +1,29 @@
-program print 
+program print
+
+    ! STRING VARIABLES
     character*20 :: name
+    character*50 :: question
+    character*3  :: confirmation
+
+    ! PRINTING TO SCREEN
     print *, "What's your name"
-    read  *, name
-    print *, "Your name is ", name
+
+    ! STRING CONCATENATION (//)
+    ! ASSIGNMENT (=)
+    read *, name 
+    question = "Your said your name is "// name //"?"
+
+    print *, question
+    read  *, confirmation
+    ! if (confirmation == "yes") .OR. (confirmation == "y")
+    !     print *, "awesome"
+    !     print *, "Your name is ", name
+    ! else (if confirmation == "no" .OR. "n")
+    !     print *, "Please type in the right name"
+    !     read *, name 
+    !     print *, "Your name is ", name
+    ! else 
+    !     print *, "Sorry, I didn't recognize your confirmation, I was expecting yes or y and no or n only"
+    ! end if 
+
 end program
