bc20649b2f26c4c2ba176aecc9880578e992d3c8
rasheedmhd <starletgh@gmail.com>
2024-10-27 13:03:47 +0000

    Looping in Fortran

--- Fortran/interactive.f90
+++ Fortran/interactive.f90
@@ -14,16 +14,30 @@ program print
     question = ">> Your said your name is "// name //"?"
 
     print *, question
+    print *, ">> Type 'yes' if correct or 'no' to correct it."
     read  *, confirmation
-    if (confirmation .eq. "yes") then
-        print *, ">> awesome"
-        print *, ">> Your name is ", name
-    else if (confirmation .eq. "no") then
+    
+    ! if (confirmation .eq. "yes") then
+    !     print *, ">> awesome"
+    !     print *, ">> Your name is ", name
+    ! else if (confirmation .eq. "no") then
+    !     print *, ">> Please type in your correct name"
+    !     read *, name 
+    !     print *, ">> Your name is ", name
+    ! else 
+    !     print *, ">> Sorry, I didn't recognize your confirmation, I was expecting yes or no only"
+    ! end if 
+
+    ! LOOPING 
+    do  while (confirmation .eq. "no")
         print *, ">> Please type in your correct name"
-        read *, name 
-        print *, ">> Your name is ", name
-    else 
-        print *, ">> Sorry, I didn't recognize your confirmation, I was expecting yes or no only"
-    end if 
+        read  *, name 
+        print *, ">> Your said your name is "// name //"?"
+        read  *, confirmation
+    end do
+
+    print *, ">> awesome"
+    print *, ">> Your name is "// name //"."
+    
 
 end program
